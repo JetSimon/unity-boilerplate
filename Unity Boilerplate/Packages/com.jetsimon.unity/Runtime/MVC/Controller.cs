cdd0f47a69301b60dffd57841af4d253cf1c09d3
Jet Simon <jetjdsimon@gmail.com>
2025-06-26 18:39:21 -0700

    Update Controller.cs

--- Unity Boilerplate/Packages/com.jetsimon.unity/Runtime/MVC/Controller.cs
+++ Unity Boilerplate/Packages/com.jetsimon.unity/Runtime/MVC/Controller.cs
@@ -3,16 +3,15 @@
     public class Controller<TModelType, TViewType> where TModelType : Model where TViewType : View
     {
         public TModelType Model { get; private set; }
-        public TViewType View { get; private set; }
-
+  
         public Controller(TModelType model)
         {
             Model = model;
         }
 
-        public virtual void BindToView(TViewType view)
+        public virtual void BindView(TViewType view)
         {
-            View = view;
+            
         }
     }
 }
